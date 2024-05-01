3900e16e68662b54518d13c6fdaf08c7055124b4
Hasnain <mrajhasnain101@gmail.com>
2024-05-01 17:59:18 +0600

    filles were added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Route,
+  BrowserRouter as Router,
+  RouterProvider,
+  Routes,
+  createBrowserRouter,
+} from "react-router-dom";
+import "./App.css";
+import { Dashboad } from "./page/Dashboad";
+import { Layout } from "./layout/Layout";
+import ShowData from "./components/ShowData";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Dashboad />,
+      },
+      {
+        path: "/view",
+        element: <ShowData />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/page/Dashboad.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Dashboad = () => {
+  return (
+    <div class="p-4">
+      <h1 class="text-2xl font-bold text-red-800">Welcome to my dashboard!</h1>
+      <p class="mt-2 text-gray-600">
+        This is an example dashboard using Tailwind CSS.
+      </p>
+    </div>
+  );
+};
